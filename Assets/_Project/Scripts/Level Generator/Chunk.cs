5bcc0c759c642a01ee8cda331ceff8191c785bf0
Daniel <danvalleribeiro@gmail.com>
2026-03-30 15:32:59 -0300

    Coins Spawns

--- Assets/_Project/Scripts/Level Generator/Chunk.cs
+++ Assets/_Project/Scripts/Level Generator/Chunk.cs
@@ -6,33 +6,86 @@ public class Chunk : MonoBehaviour
     private const float RightLane = 3f;
     private const float MiddleLane = 0f;
 
+    private const float ApplePercent = 0.3f;
+    private const float CoinPercent = 0.7f;
+
+    private const int MaxCoinsPerChunk = 5;
+    private const int MinCoinsPerChunk = 1;
+    private const float CoinLaneSpacing = 2f;
+
     [SerializeField] private float[] lanesPosition = { LeftLane, MiddleLane, RightLane };
+
     [SerializeField] private GameObject fencePrefab;
     [SerializeField] private GameObject applePrefab;
+    [SerializeField] private GameObject coinPrefab;
+
     private readonly List<int> _availableLanes = new() { 0, 1, 2 };
 
     private void Start()
     {
-        SpawnFence();
+        SpawnFences();
+        SpawnApple();
+        SpawnCoins();
     }
-    private void SpawnFence()
+    private void SpawnFences()
     {
-        int fencesToSpawn = Random.Range(0, _availableLanes.Count); // number of fences in a chunk
+        int fencesToSpawn = Random.Range(0, lanesPosition.Length); // number of fences in a chunk
+
         for (int i = 0; i < fencesToSpawn; i++)
         {
-            SpawnAtSelectedLane(fencePrefab);
+            if (_availableLanes.Count <= 0)
+            {
+                break;
+            }
+
+            int selectedLane = SelectLane();
+            SpawnAtSelectedLane(fencePrefab, selectedLane);
         }
     }
 
     private void SpawnApple()
     {
-        SpawnAtSelectedLane(applePrefab);
+        if (Random.value > ApplePercent || _availableLanes.Count <= 0)
+        {
+            return;
+        }
+
+        int selectedLane = SelectLane();
+        SpawnAtSelectedLane(applePrefab, selectedLane);
     }
 
-    private void SpawnAtSelectedLane(GameObject prefab)
+    private void SpawnCoins()
     {
+        if (Random.value > CoinPercent || _availableLanes.Count <= 0)
+        {
+            return;
+        }
+
         int selectedLane = SelectLane();
+        float topOfChunk = transform.position.z + CoinLaneSpacing * 2f;
+        int coinsPerChunk = Random.Range(MinCoinsPerChunk, MaxCoinsPerChunk); // 1 - 5
+        for (int i = 0; i < coinsPerChunk; i++)
+        {
+            if (_availableLanes.Count <= 0)
+            {
+                break;
+            }
+
+            SpawnAtSelectedLane(coinPrefab, selectedLane, i, topOfChunk);
+        }
+    }
+
+    private void SpawnAtSelectedLane(GameObject prefab, int selectedLane)
+    {
+
         Vector3 spawnPosition = new(lanesPosition[selectedLane], transform.position.y, transform.position.z);
+        Instantiate(prefab, spawnPosition, Quaternion.identity, transform);
+    }
+
+    private void SpawnAtSelectedLane(GameObject prefab, int selectedLane, int iterator, float topOfChunkZ)
+    {
+
+        Vector3 spawnPosition = new(lanesPosition[selectedLane], transform.position.y, topOfChunkZ - iterator * CoinLaneSpacing);
         Instantiate(prefab, spawnPosition, Quaternion.identity, transform);
     }
 
